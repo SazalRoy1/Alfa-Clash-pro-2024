3350c81679f760ca96230b3c3a1515f5caf9d14e
SazalRoy1 <rabidroy981@gmail.com>
2024-07-14 12:05:26 +0600

    set alphabet

--- scripts/alfa-clash.js
+++ scripts/alfa-clash.js
@@ -14,29 +14,50 @@ function play(){
 
 }
 
- 
 function getRandomAlphabet(){
-    // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('');
-    // console.log(alphabets);
-    
-    // get a  random index between 0 -25
+
+
     const randomNumber = Math.random() * 25
-   const index = Math.round(randomNumber)
-//    console.log(index)
-
-   const alphabet = alphabets[index]
-//    console.log(index, alphabet)
-   return alphabet
-
+    const index = Math.round(randomNumber)
+    const alphabet = alphabets[index]
+    return alphabet
 }
+
+ 
+// function getRandomAlphabet(){
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+    
+//     // get a  random index between 0 -25
+//     const randomNumber = Math.random() * 25
+//    const index = Math.round(randomNumber)
+// //    console.log(index)
+
+//    const alphabet = alphabets[index]
+// //    console.log(index, alphabet)
+//    return alphabet
+
+// }
 
 
 function continueGame(){
     // step-1: genrate a random alphabet
    const alphabet = getRandomAlphabet()
    console.log( 'Your random alphabet', alphabet)
+
+
+   //set randoom  randomly generated to the screen
+   const currentAlphabetElement = document.getElementById('current-alphabet')
+   currentAlphabetElement.innerText = alphabet
+   setBackGroundColorByID(alphabet)
 }
 
 
+function setBackGroundColorByID(element){
+    const elemnt = document.getElementById(element)
+    elemnt.classList.add('bg-orange-400');
+}
